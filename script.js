e0634d15180d9ef11fc6756769d04baa5d714808
Alobyte <albert.arrieta12@gmail.com>
2020-05-06 17:58:29 -0600

    Test 2

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function mailMe(){
+    window.location.href = "mailto:albert.arrieta12@gmail.com";
+}
+
+function github(){
+    window.location.href="https://github.com/Alobyte";
+}
+
+function linkedin(){
+    window.location.href="https://www.linkedin.com/in/albert-arrieta/";
+}
