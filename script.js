2cbdde515dde28a87e84a1e25424d37e5fa996a0
Alobyte <albert.arrieta12@gmail.com>
2020-05-10 20:18:36 -0600

    Newone

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+console.log("           _      ____  ______ _____ _______            _____  _____  _____ ______ _______       
+/\   | |    |  _ \|  ____|  __ \__   __|     /\   |  __ \|  __ \|_   _|  ____|__   __|/\    
+/  \  | |    | |_) | |__  | |__) | | |       /  \  | |__) | |__) | | | | |__     | |  /  \   
+/ /\ \ | |    |  _ <|  __| |  _  /  | |      / /\ \ |  _  /|  _  /  | | |  __|    | | / /\ \  
+/ ____ \| |____| |_) | |____| | \ \  | |     / ____ \| | \ \| | \ \ _| |_| |____   | |/ ____ \ 
+/_/    \_\______|____/|______|_|  \_\ |_|    /_/    \_\_|  \_\_|  \_\_____|______|  |_/_/    \_\
+                                                                                            
+                                                                                            ");
+
 
 function mailMe(){
     window.location.href = "mailto:albert.arrieta12@gmail.com";
@@ -9,4 +18,12 @@ function github(){
 
 function linkedin(){
     window.location.href="https://www.linkedin.com/in/albert-arrieta/";
+}
+
+function carsweb(){
+    window.location.href="http://goptech.ca/";
+}
+
+function carsGit(){
+    window.location.href="https://github.com/IvanOkh/Volunteer-Management-System-Frontend";
 }
